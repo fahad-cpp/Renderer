5e7767fd96c591ccc7df2e353276f511716c0a3b
Fahad Shaikh <shaikh.fahad.77377@gmail.com>
2026-07-07 13:36:00 +0530

    Fix Linux Crash

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 CONFIG="Release"
+mkdir -p build
 rm bin/$CONFIG/Renderer
 cmake -S . -G Ninja -B build -DCMAKE_BUILD_TYPE=$CONFIG -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON 2>build/error.txt
 cmake --build build --config $CONFIG --parallel >build/error.txt
-
 ./bin/$CONFIG/Renderer
